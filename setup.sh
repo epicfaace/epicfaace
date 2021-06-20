068350a1b463d86f48181cd110f8787bd18235c6
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-06-20 09:13:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 # Set up a new computer
 
-sudo apt-get install libbz2-dev
+# pyenv; see https://github.com/pyenv/pyenv/wiki
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash
